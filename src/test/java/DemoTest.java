6e534a3649246524fd1a92f578e301e06617ed56
TW-Dev-Li_Zhong <lzhong@thoughtworks.com>
2019-05-12 18:24:00 +0800

    first commit

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DemoTest {
+    @Test
+    void firstTest() {
+        assertEquals(2, 2);
+    }
+}
